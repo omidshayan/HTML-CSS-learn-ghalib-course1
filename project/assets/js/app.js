19c0bed746a06178a1e6eeec84a3e699c3f4e4b3
omidshayan <omidshayan.dev@gmail.com>
2025-02-02 11:53:33 +0430

    add new btn

--- project/assets/js/app.js
+++ project/assets/js/app.js
@@ -1,19 +1,19 @@
 let icon = document.querySelector('.icon-bar')
 let menu = document.querySelector('.menu')
 let overlay = document.querySelector('.overlay')
-let close = document.querySelector('.overlay')
+let close_menu = document.querySelector('.close-menu')
 
 function headerMenu(){
     if(icon.classList.contains('fa-bars'))
         {
             menu.style.left = 0
             overlay.style.left = 0
-            icon.classList = 'fa fa-times icon-bar';
+            icon.classList = 'fa fa-times icon-bar cursor-p';
         }
         else{
             menu.style.left = '-200px'
             overlay.style.left = '-100%'
-            icon.classList = 'fa fa-bars icon-bar';
+            icon.classList = 'fa fa-bars icon-bar cursor-p';
             
         }
 }
@@ -29,4 +29,9 @@ overlay.addEventListener('click', function(){
 
 })
 
+close_menu.addEventListener('click', function(){
+    
+    headerMenu();
+
+})
 
